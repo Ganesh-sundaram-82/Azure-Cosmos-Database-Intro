b5a7079f7a7b946fbf0f9847f2729b34c062a5eb
ganesh-sundaram-82 <gan.murs@gmail.com>
2020-04-18 16:28:34 -0500

    Adding solutions

---
+++ CosmosDB-MongoDB/MongoDBOperations.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Security.Authentication;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+using MongoDB.Driver;
+
+namespace CosmosDB.MongoDB
+{
+    public class MongoDBOperations
+    {
+        string CosmoDBuri = ConfigurationManager.AppSettings["MongoDBUri"];
+        string connectionString = @"mongodb://azure-cosmodb-mongodb-api:xTF4pDBtjZi3h8dZKY892Y1lLafyN9uGqqvcJtdZ5WVx9w6xagCIP4MFalVaVTyX02ttNi9HyfDHrcLtJJlRBg==@azure-cosmodb-mongodb-api.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@azure-cosmodb-mongodb-api@";
+        MongoClientSettings settings = null;
+        MongoClient mongoClient = null;
+
+        public MongoDBOperations()
+        {
+            settings = MongoClientSettings.FromUrl(new MongoUrl(connectionString));
+            settings.SslSettings =
+              new SslSettings() { EnabledSslProtocols = SslProtocols.Tls12 };
+            mongoClient = new MongoClient(settings);
+        }
+
+        public async Task GetAllDatabases()
+        {
+            var response = mongoClient.ListDatabases().ToList();
+            
+            ////foreach (MongoDB.Bson.BsonDocument d in response)
+            ////{
+            ////    Console.WriteLine(d.ToString());
+            ////}
+
+            ////foreach (var item in response.Result.ToList())
+            ////{
+            ////    Console.WriteLine(item.ToString());
+            ////}
+        }
+    }
+}
--- CosmosDB/Program.cs
+++ CosmosDB/Program.cs
@@ -7,21 +7,26 @@ namespace CosmosDB
     {
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Process Started..");
 
-            CosmoDBOperations cosmoDBOperations = new CosmoDBOperations();
-            Console.WriteLine("Insert into Cosmo Database");
-            Console.WriteLine();
-            await cosmoDBOperations.AddEmployeesToCosmos();
+            ////CosmoDBOperations cosmoDBOperations = new CosmoDBOperations();
+            ////Console.WriteLine("Insert into Cosmo Database");
+            ////Console.WriteLine();
+            ////await cosmoDBOperations.AddEmployeesToCosmos();
 
-            Console.WriteLine();
-            Console.WriteLine("******************************************************************");
+            ////Console.WriteLine();
+            ////Console.WriteLine("******************************************************************");
 
-            await cosmoDBOperations.GetEmployeeCollectionsFromCosmos();
+            ////await cosmoDBOperations.GetEmployeeCollectionsFromCosmos();
 
-            Console.WriteLine();
-            Console.WriteLine("******************************************************************");
+            ////Console.WriteLine();
+            ////Console.WriteLine("******************************************************************");
 
+
+            CosmosDB.MongoDB.MongoDBOperations mongoDBOperations = new MongoDB.MongoDBOperations();
+            await mongoDBOperations.GetAllDatabases();
+
+            Console.WriteLine("Process Completed..");
             Console.Read();
         }
     }
